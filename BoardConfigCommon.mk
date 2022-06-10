285f829f1a601f285885b29329cdcd691eae22a7
Kuba Wojciechowski <nullbytepl@gmail.com>
2022-06-10 23:06:28 +0800

    sm8350-common: Disable kpti
    * As per qualcomm - "SM8150/SM8250/SM8350/SM7250/SM7150/SM6150 - KPTI Not
    required".
    * It can also help increase performance by a lot in some scenarios.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,6 +116,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=0
 BOARD_KERNEL_CMDLINE += pcie_ports=compat
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
+BOARD_KERNEL_CMDLINE += kpti=off
 
 # LiveDisplay
 ifneq ("$(wildcard hardware/lineage/livedisplay)", "")
